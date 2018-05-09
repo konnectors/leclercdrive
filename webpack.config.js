1560fb7a2914aae8ce3100a18890187774093290
doubleface <christophe@cozycloud.cc>
2018-05-09 16:04:36 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+const entry = require('./package.json').main
+
+module.exports = {
+  entry,
+  target: 'node',
+  mode: 'none',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: 'manifest.konnector' },
+      { from: 'package.json' },
+      { from: 'README.md' },
+      { from: 'assets' },
+      { from: 'LICENSE' }
+    ])
+  ]
+}
